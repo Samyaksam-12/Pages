43f3b47316059a889f15409609692a875e6cb14b
vaibhav wagh <117511698+vaibhavwagh19@users.noreply.github.com>
2023-07-17 15:54:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,19 +11,7 @@ const password2 = document.getElementById('password2').value;
 
 	if (name==""){
 		document.getElementById('name-error').innerHTML = "Name field can't be left Empty.";
-		if (username==""){
-			document.getElementById('username-error').innerHTML = "username field can't be left Empty.";
-			if (email==""){
-				document.getElementById('email-error').innerHTML = "E-mail field can't be left Empty."
-				if (password==""){
-					document.getElementById('pass-error').innerHTML = "Password field can't be left Empty.";
-					if (password2==""){
-						document.getElementById('pass2-error').innerHTML = "Confirm Password field can't be left Empty.";
-						return false;
-					}
-				}
-			}
-		}
+		
 	}
 	else{
 		document.getElementById('name-error').innerHTML = "";
@@ -31,30 +19,17 @@ const password2 = document.getElementById('password2').value;
 	
 	if (username==""){
 		document.getElementById('username-error').innerHTML = "username field can't be left Empty.";
-		if (email==""){
-			document.getElementById('email-error').innerHTML = "E-mail field can't be left Empty."
-			if (password==""){
-				document.getElementById('pass-error').innerHTML = "Password field can't be left Empty.";
-				if (password2==""){
-					document.getElementById('pass2-error').innerHTML = "Confirm Password field can't be left Empty.";
-					return false;
-				}
-			}
-		}
+		
 	}
 	else{
 		document.getElementById('username-error').innerHTML = "";
 	}
 
+	//email validation
+
 	if (email==""){
 		document.getElementById('email-error').innerHTML = "E-mail field can't be left Empty."
-		if (password==""){
-			document.getElementById('pass-error').innerHTML = "Password field can't be left Empty.";
-			if (password2==""){
-				document.getElementById('pass2-error').innerHTML = "Confirm Password field can't be left Empty.";
-				return false;
-			}
-		}
+		
 	}
 	else{
 		if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.email.value)){
@@ -66,46 +41,41 @@ const password2 = document.getElementById('password2').value;
 		}
 	}
 
+
+	// password validation
+
 	if (password==""){
 		document.getElementById('pass-error').innerHTML = "Password field can't be left Empty.";
-		if (password2==""){
-			document.getElementById('pass2-error').innerHTML = "Confirm Password field can't be left Empty.";
-			return false;
-		}
-		else{
-			document.getElementById('pass2-error').innerHTML = "";
-			return false;
-		}
+		
 	}
 	else{
 		document.getElementById('pass-error').innerHTML = "";
+		if(password.length<6){
+			document.getElementById('pass-error').innerHTML = "Password length must be Greater than 6.";
+			return false;
+		}
+		else if(password.length>6){
+			document.getElementById('pass-error').innerHTML = "";
+			return false;
+		}
 	}
 	
 	
-	if(password.length<6){
-		document.getElementById('pass-error').innerHTML = "Password length must be Greater than 6.";
+	if (password2==""){
+		document.getElementById('pass2-error').innerHTML = "confirm password field can't be left Empty.";
 		return false;
 	}
-	else if(password.length>6){
-		document.getElementById('pass-error').innerHTML = "";
+	else if(password!=password2){
+		document.getElementById('pass2-error').innerHTML ="Password not Mached.";
 		return false;
 	}
 	else{
-		if (password2==""){
-			document.getElementById('pass2-error').innerHTML = "confirm password field can't be left Empty.";
+		document.getElementById('pass2-error').innerHTML ="Password Mached.";
+		if(form.checkbox.checked==false){
+			alert("please check the box.");
 			return false;
 		}
-		else if(password!=password2){
-			document.getElementById('pass2-error').innerHTML ="Password not Mached.";
-			return false;
-		}
-		else{
-			document.getElementById('pass2-error').innerHTML ="Password Mached.";
-			if(form.checkbox.checked==false){
-				alert("please check the box.");
-				return false;
-			}
-			return true;
-		}
+		return true;
 	}
+	
 }
